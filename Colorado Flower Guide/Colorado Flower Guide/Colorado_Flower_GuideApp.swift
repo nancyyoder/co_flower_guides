4248b2dd677c332233e87deb3d197710ed3c1156
Nancy Yoder <nayo4039@colorado.edu>
2022-11-16 13:04:27 -0700

    added content

---
+++ Colorado Flower Guide/Colorado Flower Guide/Colorado_Flower_GuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  Colorado_Flower_GuideApp.swift
+//  Colorado Flower Guide
+//
+//  Created by Nancy Yoder on 11/16/22.
+//
+
+import SwiftUI
+
+@main
+struct Colorado_Flower_GuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Colorado Flower Guide/Colorado Flower Guide/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Colorado Flower Guide
+//
+//  Created by Nancy Yoder on 11/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Blue Columbine")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
