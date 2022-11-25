10f46cec4ab921fd437fa943a294e90002bbfd40
Nancy Yoder <nayo4039@colorado.edu>
2022-11-25 16:39:41 -0700

    fixed bugs

--- Colorado Flower Guide/Colorado Flower Guide/Views/ContentView.swift
+++ Colorado Flower Guide/Colorado Flower Guide/Views/ContentView.swift
@@ -9,31 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack (alignment: .leading){
-            
-            BackgroundView()
-                .frame(height: 200)
-            ImageView()
-                .offset(x: 80, y: -200)
-                .padding(.bottom, -250)
-            
-            VStack(alignment: .leading){
-                Text("Colorado Blue Columbine")
-                    .font(.title)
-                Text("Aquilegia coerulea")
-                    .font(.subheadline)
-                    .foregroundColor(.secondary)
-                Divider()
-                VStack (alignment: .leading){
-                    Text("Colors: Pale Blue, White, Yellow")
-                    Text("Shapes: Star, Buttercup")
-                    Text("States: Colorado, Idaho, Montana, Wyoming, New Mexico, Utah")
-                    Text("Smells: Honeysuckle")
-                }.padding(.top)
-            }.padding(.leading, 30.0)
-        }
-        
-        Spacer()
+        FlowerListView()
     }
 }
 
